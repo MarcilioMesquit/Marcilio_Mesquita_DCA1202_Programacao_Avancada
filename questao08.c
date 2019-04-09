de1899f0af9775cb2c35ca5e6abbdaba729a2fd3
MarcilioMesquit <41900655+MarcilioMesquit@users.noreply.github.com>
2019-04-08 22:54:52 -0300

    Delete questao08.c

--- questao08.c
+++
@@ -1,3 +0,0 @@
-p = mat + 1 ;Pois tendo que os dois sao do mesmo tipo, então o ponteiro recebe um endereço do mesmo tipo.
-
-x = (*mat)++ ;A varia x do tipo inteiro recebe a posição no vetor e assim deloca para a direita.
